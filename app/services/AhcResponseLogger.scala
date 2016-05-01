c899e07522dfa824f404758524ebe4f9e581cc6e
Marcos Pereira <marcos.silva@gmail.com>
2016-05-01 00:47:40 -0300

    Better headers log

--- app/services/AhcResponseLogger.scala
+++ app/services/AhcResponseLogger.scala
@@ -4,8 +4,9 @@ import org.slf4j.LoggerFactory
 import play.api.libs.ws.{ WSRequest, WSRequestExecutor, WSRequestFilter, WSResponse }
 
 import scala.concurrent.Future
-
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+import scala.collection.immutable.Iterable
 
 case class AhcResponseLogger(
     headers: Boolean = true,
@@ -22,6 +23,7 @@ case class AhcResponseLogger(
         futureResponse.map { response =>
           logStatusCode(response)
           logHeaders(response)
+          logBody(response)
           response
         }
       }
@@ -31,7 +33,8 @@ case class AhcResponseLogger(
       }
 
       private def logHeaders(response: WSResponse) = if (headers) {
-        logger.info(response.allHeaders.map(header => s"${header._1}: ${header._2.mkString(",")}").mkString("\n"))
+        val formattedHeaders: Iterable[String] = response.allHeaders.map(header => s"${header._1}: ${header._2.mkString(",")}")
+        logger.info(formattedHeaders.mkString(start = "\n", sep = "\n", end = "\n"))
       }
 
       private def logBody(response: WSResponse) = if (body) {

1e25e8214331b4b162a82a95e8877cf40cfca296
Marcos Pereira <marcos.silva@gmail.com>
2016-04-30 23:17:22 -0300

    Remove unnecessary code

--- app/controllers/CallbacksController.scala
+++ app/controllers/CallbacksController.scala
@@ -3,8 +3,7 @@ package controllers
 import javax.inject.{ Inject, Singleton }
 
 import play.api.Configuration
-import play.api.mvc.Action
-import play.api.mvc.Controller
+import play.api.mvc.{ Action, Controller }
 
 @Singleton
 class CallbacksController @Inject() (config: Configuration) extends Controller {

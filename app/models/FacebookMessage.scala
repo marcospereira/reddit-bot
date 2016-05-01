d5f2d31c43ce248d6cdb8747902894ed54316c4c
Marcos Pereira <marcos.silva@gmail.com>
2016-05-01 02:18:28 -0300

    Handle images uploaded directly to reddit

--- app/models/FacebookMessage.scala
+++ app/models/FacebookMessage.scala
@@ -23,7 +23,7 @@ object Attachment {
       Card(
         title = post.getTitle,
         subtitle = s"From ${post.getAuthor} | ${post.getCommentCount} comments | ${post.getUpVotes} ups | ${post.getDownVotes} downs",
-        image_url = if (post.getSource != null) Some(post.getSource.getUrl) else None,
+        image_url = (if (post.getSource != null) Some(post.getSource.getUrl) else None).map(_.replaceAll("amp;", "")),
         buttons = buttons(post)
       )
     }
